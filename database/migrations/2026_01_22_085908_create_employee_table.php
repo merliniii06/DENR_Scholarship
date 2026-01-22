e6122d7c0bf50a3190b7265a373ea31ec550065f
Akame <merlinjoshuavertucio03@gmail.com>
2026-01-22 18:18:07 +0800

    Initial commit

---
+++ database/migrations/2026_01_22_085908_create_employee_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employee', function (Blueprint $table) {
+        $table->id();
+        $table->string('full_name');
+        $table->unsignedTinyInteger('age');
+        $table->string('ipcr');
+        $table->string('email');
+        $table->string('position');
+        $table->string('office');
+        $table->integer('phone_number');    
+        $table->string('file_1')->nullable();
+        $table->string('file_2')->nullable();
+        $table->string('file_3')->nullable();
+        $table->string('file_4')->nullable();
+        $table->string('file_5')->nullable();
+        $table->string('file_6')->nullable();
+        $table->string('file_7')->nullable();
+        $table->string('file_8')->nullable();
+        $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employee');
+    }
+};
